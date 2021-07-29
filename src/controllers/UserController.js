f26c8f20322f7d8b1370e3831b378d5cf091d426
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2021-07-28 22:10:15 -0300

    criando erros da rota de usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,22 @@
-import User from '../models/Aluno';
+import User from '../models/User';
 
 class UserController {
   async store(request, response) {
-    const novoUser = await User.create({
-      nome: 'Daniel',
-      sobrenome: 'dos Santos',
-      email: 'd.santosc@terra.com.br',
-      password: '123456',
-    });
+    const { nome, email, password } = request.body;
+    try {
+      const novoUser = await User.create({
+        nome,
+        email,
+        password,
+      });
 
-    return response.json(novoUser);
+      return response.json(novoUser);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
-export default UserController();
+export default new UserController();
