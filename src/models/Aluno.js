62ee678f33fef09ada6e923d937166e447f46d4f
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2021-08-09 16:13:48 -0300

    criada rotas para alunos

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,62 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'required 3 to 255 characters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Invalid Email',
+          },
+        },
+      },
+      sobrenome: {
+        type: sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'required 3 to 255 characters',
+          },
+        },
+      },
+      idade: {
+        type: sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'age must be whole number',
+          },
+        },
+      },
+      peso: {
+        type: sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'needs to be numerical',
+          },
+        },
+      },
+      altura: {
+        type: sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'needs to be numerical',
+          },
+        },
+      },
     }, {
       sequelize,
     });
--- src/routes/alunosRoutes.js
+++ src/routes/alunosRoutes.js
@@ -4,5 +4,9 @@ import alunosController from '../controllers/AlunosController';
 const routes = Router();
 
 routes.get('/', alunosController.index);
+routes.post('/', alunosController.store);
+routes.put('/', alunosController.update);
+routes.get('/', alunosController.show);
+routes.delete('/', alunosController.delete);
 
 export default routes;
