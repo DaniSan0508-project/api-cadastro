ef2bdc3b285992a424c7a59c9a4c6a2637f49f00
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2021-08-12 13:53:11 -0300

    fix:uploads apenas de imagens jpeg jpg ou png

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -2,6 +2,14 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  // verificando se é png ou jpeg
+  fileFilter: (request, file, cb) => {
+    if (file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      return cb(new multer.MulterError('file must be .png or .jpg'));
+    }
+    return cb(null, true);
+  },
+
   storage: multer.diskStorage({
     destination: (request, file, cb) => {
       cb(null, resolve(__dirname, '..', 'uploads'));
